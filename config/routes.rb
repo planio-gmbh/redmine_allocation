974cd25741f98de5042ea5a0ad91c540d1bb431b
jresinas <jresinas@emergya.com>
2013-11-18 12:25:08 +0400

    Making compatible with Redmine 2.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-ActionController::Routing::Routes.draw do |map|
-  map.allocation_by_project 'projects/:id/project_allocation', :controller => 'allocation', :action => 'by_project'
-  map.allocation_by_user 'projects/:id/user_allocation', :controller => 'allocation', :action => 'by_user'
+RedmineApp::Application.routes.draw do
+#ActionController::Routing::Routes.draw do |map|
+  match '/projects/:id/project_allocation' => 'allocation#by_project', :as => :allocation_by_project_path
+  match '/projects/:id/user_allocation' => 'allocation#by_user', :as => :allocation_by_user_path
+
+#  map.allocation_by_project 'projects/:id/project_allocation', :controller => 'allocation', :action => 'by_project'
+#  map.allocation_by_user 'projects/:id/user_allocation', :controller => 'allocation', :action => 'by_user'
 end
