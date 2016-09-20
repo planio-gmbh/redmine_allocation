a5c187516e1626d0e27bcf86355826bac0bacf41
Jens Kraemer <jk@jkraemer.net>
2016-09-20 17:50:34 +0800

    fix queries for rails 4

--- app/controllers/allocation_controller.rb
+++ app/controllers/allocation_controller.rb
@@ -4,30 +4,31 @@ class AllocationController < ApplicationController
   include Allocation::Allocation
 
   def by_project
-    @projects = Project.visible.active.find :all,
-                                            :conditions => @project.project_condition(true),
-                                            :include => [:members => :user],
-                                            :order => "#{Project.table_name}.lft"
+    @projects = Project.visible.active.
+      where(@project.project_condition(true)).
+      includes(members: :user).
+      order("#{Project.table_name}.lft")
   end
 
   def by_user
     @months = months
     user_group_field = Setting.plugin_redmine_allocation['users_custom_field']
     project_group_field = Setting.plugin_redmine_allocation['projects_custom_field']
-    subproject_users = User.active.find :all,
-                                        :select => "DISTINCT #{User.table_name}.*",
-                                        :joins => { :members => :project },
-                                        :include => :members,
-                                        :conditions => @project.project_condition(true)
+    subproject_users = User.active.where(@project.project_condition(true)).
+      select("DISTINCT #{User.table_name}.*").
+      joins(members: :project).
+      includes(:members)
+
     if user_group_field.present? and project_group_field.present?
       group = @project.custom_value_for(project_group_field)
       quoted_group = CustomValue.connection.quote group
-      group_users = User.active.find :all,
-                                     :joins => { :custom_values => :custom_field },
-                                     :include => :members,
-                                     :conditions => ["#{CustomField.table_name}.id = ?" +
-                                                    " AND #{CustomValue.table_name}.value = ?",
-                                                    user_group_field, quoted_group]
+      group_users = User.active.
+        where(
+        "#{CustomField.table_name}.id = ? AND #{CustomValue.table_name}.value = ?", user_group_field, quoted_group
+        ).
+        joins(custom_values: :custom_field).
+        includes(:members)
+
       outside_users = subproject_users - group_users
       group_allocation = allocation_by_months(group_users, @months)
       outside_allocation = allocation_by_months(outside_users, @months)
